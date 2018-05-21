625e64932ceb0dc770becfd531b34c7968394fcb
hisayohorie <hisayohorie@gmail.com>
2018-05-21 15:59:47 -0400

    Scrape every cat food product page

--- scraper.rb
+++ scraper.rb
@@ -7,17 +7,23 @@ require 'mechanize'
 agent = Mechanize.new
 #
 # # Read in a page
-page_url = "http://performatrin.com/products/performatrin-ultra-grain-free-recipe-cat-food/#"
+base_url = "http://performatrin.com/products-selector/"
+base_page = agent.get(base_url)
 
-page = agent.get(page_url)
-page.at("#analysis_content").search('tr').each do |tr|
-  key = tr.search('th').text
-  value = tr.search('td').text
-  info = {
-    nutrition: key,
-    value: value
-  }
-  ScraperWiki.save_sqlite([:nutrition], info)
+product_urls = base_page.at("#products-listed").search("a").map {|a| a.attributes["href"].value }
+cat_product_urls = product_urls.select {|url| url.include? "cat"}
+cat_product_urls.each do |url|
+  product_name = url.split("/").last.gsub("-", "_")
+  page = agent.get(url)
+  page.at("#analysis_content").search('tr').each do |tr|
+    key = tr.search('th').text
+    value = tr.search('td').text
+    info = {
+      nutrition: key,
+      value: value
+    }
+    ScraperWiki.save_sqlite([:nutrition], info,table_name=product_name)
+  end
 end
 
 # # Find somehing on the page using css selectors
